c095186e767af38ec6c6010e6a326621aee3794d
Atribu <oguzkilinc.ant@gmail.com>
2026-06-19 15:31:14 +0300

    v6
    v6

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    proxyTimeout: 300_000,
+    serverActions: {
+      bodySizeLimit: "300mb",
+    },
   },
 };
 
